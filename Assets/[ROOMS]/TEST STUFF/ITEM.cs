002c05b260dba24a739bf13879f388374d4ce86b
XP-Asra <tenmapegaso1@gmail.com>
2025-05-22 13:52:09 -0600

    SE MOVIERON SCRIPTS A NUEVA CARPTA Y ELIMINARON SIMILARES

--- Assets/[ROOMS]/TEST STUFF/ITEM.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "Items/New Item", fileName = "Item_")]  // Ruta clara en el menú
-public class ITEM : ScriptableObject
-{
-    public string _name;
-    public Sprite _sprite;  // Icono del ítem
-    public GameObject _prefab;  // Prefab opcional para instanciar
-}
